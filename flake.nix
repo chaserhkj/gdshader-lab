8aeb7ecaa08424aeab5e2df3e038966e477fcd00
Chaser Huang <huangkangjing@gmail.com>
2025-06-08 23:52:55 -0400

    Use automatic flake modules

--- flake.nix
+++ flake.nix
@@ -3,13 +3,19 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs-unstable";
+    make-shell.url = "github:nicknovitski/make-shell";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-      ];
+      imports = let
+        local-module-dir = ./flake-modules;
+        local-modules = with builtins; map (
+          modName: local-module-dir + ("/" + modName)
+        ) (attrNames (readDir local-module-dir));
+      in [
+      ] ++ local-modules;
       systems = [ "x86_64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
       };
