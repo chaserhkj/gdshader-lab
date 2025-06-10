7ba9cf597bd10fc15db8268025038320d274fba5
Chaser Huang <huangkangjing@gmail.com>
2025-06-09 21:45:58 -0400

    Add libHLSLcc from unity

--- flake-modules/packages.nix
+++ flake-modules/packages.nix
@@ -3,6 +3,7 @@
     packages = {
       fxc2-exe = pkgs.callPackage ../nix-packages/fxc2-exe.nix {};
       fxc2-wrapper = pkgs.callPackage ../nix-packages/fxc2-wrapper.nix {inherit (self'.packages) fxc2-exe;};
+      libhlslcc = pkgs.callPackage ../nix-packages/libhlslcc.nix {};
     };
   };
 }
---
+++ nix-packages/libhlslcc.nix
@@ -0,0 +1,14 @@
+{
+  stdenv,
+  cmake,
+  fetchFromGitHub,
+} : stdenv.mkDerivation {
+  pname = "hlslcc";
+  version = "rolling";
+  src = fetchFromGitHub {
+    owner = "Unity-Technologies";
+    repo = "HLSLcc";
+    rev = "3ea1fcd6bd0ac445bc078de0bf32f0950188577b";
+  };
+  nativeBuildInputs = [cmake];
+}
