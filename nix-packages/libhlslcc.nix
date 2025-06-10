696973572255e0c49c8812273576079ac23e39d7
Chaser Huang <huangkangjing@gmail.com>
2025-06-09 21:49:37 -0400

    Update hash for libHLSLcc

--- nix-packages/libhlslcc.nix
+++ nix-packages/libhlslcc.nix
@@ -9,6 +9,7 @@
     owner = "Unity-Technologies";
     repo = "HLSLcc";
     rev = "3ea1fcd6bd0ac445bc078de0bf32f0950188577b";
+    hash = "sha256-i+ZN3bE+RgAk4NrUr8Pmi0wLqAslE9ZMQTHnukNLhpE=";
   };
   nativeBuildInputs = [cmake];
 }
