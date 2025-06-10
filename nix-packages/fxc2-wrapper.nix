fcdf3e751d4ad595ee0f13787507b6cace08e8af
Chaser Huang <huangkangjing@gmail.com>
2025-06-09 20:33:58 -0400

    Some improvements to fxc2-wrapper

--- nix-packages/fxc2-wrapper.nix
+++ nix-packages/fxc2-wrapper.nix
@@ -3,6 +3,9 @@
   wine,
   fxc2-exe,
   ...}: writeShellScriptBin "fxc2" ''
+# Suppress console outputs
 export WINEDEBUG=-all
+# Suppress user interaction by unsetting DISPLAY
+unset DISPLAY
 exec ${wine}/bin/wine ${fxc2-exe}/fxc2.exe "$@"
 ''
