1ed72effe5a6c33da25f9056edb601759692905c
Chaser Huang <huangkangjing@gmail.com>
2025-06-09 20:36:24 -0400

    Use different wine prefix in wrapper

--- nix-packages/fxc2-wrapper.nix
+++ nix-packages/fxc2-wrapper.nix
@@ -5,6 +5,8 @@
   ...}: writeShellScriptBin "fxc2" ''
 # Suppress console outputs
 export WINEDEBUG=-all
+# Use a different default wine prefix
+[[ -z "$WINEPREFIX" ]] && export WINEPREFIX=$HOME/.fxc2-wine
 # Suppress user interaction by unsetting DISPLAY
 unset DISPLAY
 exec ${wine}/bin/wine ${fxc2-exe}/fxc2.exe "$@"
